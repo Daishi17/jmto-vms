d9e17aeb4ef844661a17375f721eda3064089b7f
unknown <danang1724@gmail.com>
2023-06-04 21:05:53 +0700

    pemindahan ui

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,233 @@
+<section class="content">
+
+    <!-- Default box -->
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h5 class="card-title">
+                <span class="text-secondary">
+                    <i class="fas fa-user-tag"></i>
+                    <strong>SISTEM INFORMASI DATA REKANAN TERVALIDASI (SI-DRT)</strong>
+                </span>
+            </h5>
+        </div>
+        <div class="card-body">
+            <div class="row mb-2">
+                <div class="col-md-3">
+                    <div class="card card-primary card-outline">
+                        <div class="card-body box-profile">
+                            <div class="text-center">
+                                <img class="profile-user-img img-fluid img-circle" src="<?php echo base_url(); ?>assets/template/frontend/dist/img/logo usaha.png" alt="User profile picture">
+                            </div>
+                            <h5 class="profile-username text-center text-sm">
+                                <strong>Kreatif Intelegensi Teknologi</strong>
+                            </h5>
+                            <p class="text-muted text-center">Jasa Lainnya || Jasa Konsultasi || Jasa Pemborongan</p>
+                            <ul class="list-group list-group-unbordered mb-3">
+                                <li class="list-group-item">
+                                    <b>Kualifikasi Usaha</b> <a class="float-right">Menengah</a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>NPWP</b> <a class="float-right">95.725.637.3-411.000</a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>Email</b> <a class="float-right">kreatifintelegensi@gmail.com</a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>Status Dokumen</b>
+                                    <a class="float-right">
+                                        <span class="badge badge-danger">
+                                            <strong>Belum Lengkap</strong>
+                                        </span>
+                                    </a>
+                                </li>
+                                <li class="list-group-item">
+                                    <b>Status Tender Terundang</b>
+                                    <a class="float-right">
+                                        <span class="badge badge-warning">
+                                            <strong>Belum Tervalidasi</strong>
+                                        </span>
+                                    </a>
+                                </li>
+                                </u>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="card card-primary card-outline">
+                        <div class="card-header">
+                            <h5 class="card-title">
+                                <span>
+                                    <i class="fas fa-user-tag"></i>
+                                    <strong>Informasi Usaha / Perorangan</strong>
+                                </span>
+                            </h5>
+                        </div>
+                        <div class="card-body">
+                            <form>
+                                <table class="table table-bordered table-sm">
+                                    <tr>
+                                        <th class="col-sm-3 bg-light">
+                                            Bentuk Usaha
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-building mr-2"></i>
+                                            Perseroan Terbatas (PT)
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Alamat
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-road mr-2"></i>
+                                            Jl. Kodiklat TNI No. 15 Raya Puspitek
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Provinsi
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-map mr-2"></i>
+                                            Banten
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Kabupaten / Kota
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-map-signs mr-2"></i>
+                                            Tangerang Selatan
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Kecamatan
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-route mr-2"></i>
+                                            Serpong
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Kelurahan
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-map-marker-alt mr-3"></i>
+                                            Buaran
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Kode Pos
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-map-pin mr-3"></i>
+                                            15310
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Kontak
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-phone-alt mr-2"></i>
+                                            0811-8333-433
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Kantor Cabang
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <i class="fas fa-laptop-house mr-2"></i>
+                                            Tidak Ada
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            Alamat Kantor Cabang
+                                        </th>
+                                        <td class="col-sm-10">
+                                            -
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="col-sm-2 bg-light">
+                                            <span class="text-info">More Options</span>
+                                        </th>
+                                        <td class="col-sm-10">
+                                            <span type="button" class="badge badge-info">
+                                                <strong>
+                                                    <a href="<?= base_url('datapenyedia/identitas_perusahaan') ?>" class="nav-link">
+                                                        <span class="text-white"><i class="fas fa-edit mr-2"></i>Edit Changes</span>
+                                                    </a>
+                                                </strong>
+                                            </span>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card card-danger card-outline">
+                <div class="card-header">
+                    <h5 class="card-title">
+                        <span>
+                            <i class="fas fa-chart-pie"></i>
+                            <strong>Informasi Grafis Rekanan</strong>
+                        </span>
+                    </h5>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3 col-sm-6 col-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-danger"><i class="far fa-envelope"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text"><strong>Dokumen Tervalidasi</strong></span>
+                                    <span class="info-box-text text-danger"><strong>8 / 10</strong></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-warning"><i class="fas fa-percentage"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text"><strong>Penilaian Kinerja</strong></span>
+                                    <span class="info-box-text"><strong>74.82</strong></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-success"><i class="fas fa-bullhorn"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text"><strong>Tender Yang di Ikuti</strong></span>
+                                    <span class="info-box-text text-success"><strong>11</strong></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 col-12">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-primary"><i class="fas fa-marker"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text"><strong>Tender Berkontrak</strong></span>
+                                    <span class="info-box-text text-primary"><strong>3</strong></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
